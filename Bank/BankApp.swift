0d7a2f2ba20396d62fdbd9496d4d50d49fe0fd6c
Ali Mert Özhayta <ozhayta@zoho.com>
2022-05-02 23:10:39 +0300

    Initial Commit

---
+++ Bank/BankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankApp.swift
+//  Bank
+//
+//  Created by Ali Mert Özhayta on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct BankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
